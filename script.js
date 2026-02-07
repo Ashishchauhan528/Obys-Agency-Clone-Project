18435bbcd58203816e45d8f0866acbb04494f9d3
Ashish <chauhanashish0831@gmail.com>
2026-02-08 00:04:05 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var tl = gsap.timeline()
+function loadingAnimation(){
+    var tl = gsap.timeline()
 tl.from(".line h1",{
     y:150,
     stagger:0.25,
     duration:0.6,
     delay:0.5
-})
-tl.from("#line1-part1 , .line h2",{
+});
+tl.from("#line1-part1",{
     opacity:0,
     onStart: function(){
         var h5timer = document.querySelector("#line1-part1 h5")
@@ -18,10 +19,52 @@ tl.from("#line1-part1 , .line h2",{
         }
     },35)
     }
+});
+tl.to(".line h2",{
+    animationName: "anime",
+    opacity:1
 })
-
 tl.to("#loader",{
     opacity:0,
     duration:0.4,
-    delay:4
+    delay:1
+});
+
+tl.from("#page1",{
+    delay:0.2,
+    duration:0.5,
+    y:1600,
+    opacity:0,
+    ease:Power4
 })
+
+tl.to("#loader",{
+    display:"none"
+})
+
+tl.from("#nav",{
+    opacity:0
+})
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:120,
+    stagger:0.1
+})
+}
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+        left:dets.x,             // isse mouse ke sath vo circle bhi chalega.
+        top:dets.y
+    })
+})
+
+// Apply Shery magnet effect when page loads
+window.addEventListener("load", function(){
+    setTimeout(function(){
+        Shery.makeMagnet("#nav-part2 h4", {});
+    }, 1000);
+});
+}
+
+loadingAnimation()
+cursorAnimation()
